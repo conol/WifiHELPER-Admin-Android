186e97f29b403e29bdf4e1a9b81e02dcbb181b5e
itouuuuuuuuu <mfito.13@gmail.com>
2017-10-15 03:29:59 +0900

    モジュール組み込み

--- app/src/main/java/jp/co/conol/wifihelper_admin_android/MainActivity.java
+++ app/src/main/java/jp/co/conol/wifihelper_admin_android/MainActivity.java
@@ -1,16 +1,25 @@
 package jp.co.conol.wifihelper_admin_android;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ConstraintLayout mScanBackgroundConstraintLayout;
+    private ConstraintLayout mScanDialogConstraintLayout;
+    private ConstraintLayout mConnectingProgressConstraintLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScanBackgroundConstraintLayout = (ConstraintLayout) findViewById(R.id.ScanBackgroundConstraintLayout);
+        mScanDialogConstraintLayout = (ConstraintLayout) findViewById(R.id.scanDialogConstraintLayout);
+        mConnectingProgressConstraintLayout = (ConstraintLayout) findViewById(R.id.connectingProgressConstraintLayout);
     }
 
     public void onAppAboutTextViewTapped(View view) {
